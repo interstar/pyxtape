349a45deba479d2213a8970a604dcb0d69cd1290
phil jones <interstar@gmail.com>
2016-01-23 19:50:04 -0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # pyxtape
+
 Make "mixtapes" with Python, sox and avconv
+
+## Why?
+
+I wanted to sequence a bunch of mp3s and other audio files together into a single mixed file. Without using awkward DJing software of even something as heavy as Audacity. A mix is a sequence of music with short cross-fades between each one. There's no attempt at beat-matching or other clever synchronization, but you can determine how much of a cross-fade you end up with.
+
+## Workflow
+
+1) I use [Totem](https://wiki.gnome.org/Apps/Videos), the GNOME desktop movie player, to collect a playlist which I can then export as a .pls file. (Other applications seem to support the .pls format too.)
+
+The playlist file contains the full path to the music files. 
+
+2)
+    python makelist.py MYPLAYLIST.pls "1,1,2,3,5,8,13"
+    
+makelist reads the playlist file (MYPLAYLIST.pls) and then calls avconv and sox (make sure you have them installed on your machine) via the included [crossfade_cat.sh](https://github.com/jacksonh/sox/blob/master/scripts/crossfade_cat.sh) shell-script (originally from the sox site).
+
+The second argument to makelist.py, is a string containing a list of numbers separated by commas. These are the time (in seconds) for the cross-fade between each track.
+
+3) When finished (note, the process can take a long time), your tracks will have been colated into a file called mix.wav in your directory.
+
+
---
+++ makelist.py
@@ -0,0 +1,68 @@
+import sys, os, urlparse, urllib
+from subprocess import call
+
+#sox short.au long.au longer.au
+
+def munge(l) :
+    p = urlparse.urlparse((l.split("=")[1]).strip())
+    
+    #return os.path.abspath(os.path.join(p.netloc, p.path))    
+    return urllib.unquote((l.split("=")[1]).strip())[7:]
+    
+
+def takeN(n,xs) :
+    i = 0
+    while (i<(len(xs)-1)) :
+        yield xs[i],xs[i+1]
+        i=i+1
+        
+
+def getType(name) : return name.split(".")[-1]
+
+
+def makeMix(plsName, transitionTimes) :
+    trackno = 0
+
+    f = open(plsName) 
+    lines = f.readlines()[2:] 
+    paths = [munge(l) for l in lines if l[0:4]=="File"]
+    
+    print "==== Track %s :: %s" % (trackno,paths[0])
+    call(["rm","mix.wav"])
+    call(["avconv","-i",paths[0],"mix.wav"])
+    trackno=trackno+1    
+    
+    #part 1 [8,6,8,8,9,15]
+    for path,cross in zip(paths[1:],transitionTimes) :
+        print "==== Track %s :: %s" % (trackno,path)       
+        call(["mv","mix.wav","old.wav"])
+        nextType = getType(path)
+
+        if nextType in ["flac","wma"] :
+            tmp = "./tmp.wav"
+            tmp2 = "./tmp2.wav"
+            call(["rm",tmp])
+            call(["avconv","-i",path,tmp])
+        else : 
+            tmp = "./tmp.%s"%nextType                       
+            tmp2 = "./tmp2.%s"%nextType
+            call(["cp",path,tmp])                    
+       
+
+        call(["sox",tmp,"-r","44100",tmp2])
+        #call(["sox","-norm",tmp2,tmp])
+    
+        print "Merging %s with %s" % ("old.wav",tmp2)
+        
+        command = ["./crossfade_cat.sh", "%s"%cross, "old.wav", tmp2, "auto", "auto"]
+        call(command)
+        trackno=trackno+1    
+
+
+#makeMix("list2.pls",[2,4,2,2,2,2,3,3])
+if __name__ == '__main__' :
+    from sys import argv
+    mixname = argv[1]
+    trans = argv[2]
+    makeMix(argv[1],trans.split(","))
+    
